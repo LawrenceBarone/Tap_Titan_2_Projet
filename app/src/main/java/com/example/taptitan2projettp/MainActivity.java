0a6baa93fd49e42cf2d4a317a3234f8284ebcd45
Kamilikie <61969856+Kamilikie@users.noreply.github.com>
2021-01-06 18:21:02 +0100

    1er intergration De Thomas. L'image pour l'ennemie Le tap pour faire des dégât a l'ennemie Le gain d'argent apres la mort d'un ennemie Le gain de puissance apres la mort d'un ennemie (taper plus fort) Séparation en l'ecran en deux partie

--- app/src/main/java/com/example/taptitan2projettp/MainActivity.java
+++ app/src/main/java/com/example/taptitan2projettp/MainActivity.java
@@ -3,13 +3,66 @@ package com.example.taptitan2projettp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int test = 0;
+    ImageView tap;
+    TextView info,Gold;
+    RelativeLayout test;
+    ProgressBar pv;
+
+    int compteur,vie,Maxvie,puissance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tap = findViewById(R.id.image);
+        info = findViewById(R.id.Textvieuw);
+        Gold = findViewById(R.id.argent);
+        test = findViewById(R.id.linear);
+        pv = findViewById(R.id.progressBar);
+        compteur = 0;
+        Maxvie = 100;
+        vie = Maxvie;
+        pv.setMax(Maxvie);
+        pv.setProgress(vie);
+        puissance = 1;
+
+
+
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vie >=1)
+                {
+                    vie= vie-puissance;
+                    pv.setProgress(vie);
+
+                }
+                else
+                {
+                    Maxvie = Maxvie+50;
+                    vie = Maxvie;
+                    pv.setMax(Maxvie);
+                    pv.setProgress(vie);
+                    puissance = puissance+10;
+                    compteur = compteur+600;
+                }
+                info.setText("Vie: " + String.valueOf(vie));
+                Gold.setText("Gold: " + String.valueOf(compteur));
+            }
+        });
+
+
+
     }
+
 }
